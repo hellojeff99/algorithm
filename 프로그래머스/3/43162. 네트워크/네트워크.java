b03fdcf79ab0053ffd201141a2663c364ac19077
Seongwon(Jeff) <53917860+codearchitect99@users.noreply.github.com>
2026-03-05 17:30:23 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,31 +1,23 @@
-import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
-        List<Integer>[] listArr = new ArrayList[n];
         boolean[] isVisited = new boolean[n];
         int count = 0;
-        for (int i = 0; i < n; i++) listArr[i] = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-                if(computers[i][j] == 1) listArr[i].add(j);
-            }
-        }
-        Deque<Integer> stack = new ArrayDeque<>();
-        for (int i = 0; i < n; i++) {
+        
+        for (int i = 0; i < computers.length; i++) {
             if (isVisited[i]) continue;
-            isVisited[i] = true;
-            stack.push(i);
             count++;
-            while (!stack.isEmpty()) {
-                int pop = stack.pop();
-                for (int num: listArr[pop]) {
-                    if (isVisited[num]) continue;
-                    stack.push(num);
-                    isVisited[num] = true;
-                }
-            }
+            dfs(i, computers, isVisited);
         }
         return count;
+    }
+    
+    private void dfs(int cur, int[][] computers, boolean[] isVisited) {
+        isVisited[cur] = true;
+        int[] arr = computers[cur];
+        for (int next = 0; next < arr.length; next++) {
+            if (cur == next) continue;
+            if (isVisited[next]) continue;
+            if (computers[cur][next] == 1) dfs(next, computers, isVisited);
+        }
     }
 }
