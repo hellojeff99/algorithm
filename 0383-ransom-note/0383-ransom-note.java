7bf3a714854c65372fc61bd5cba58477b2e68a9c
Seongwon Yang <53917860+hellojeff99@users.noreply.github.com>
2026-03-13 16:48:02 +0900

    Time: 15 ms (34.12%), Space: 46.9 MB (44.7%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c: magazine.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c: ransomNote.toCharArray()) {
+            if (map.getOrDefault(c, 0) > 0) {
+                map.put(c, map.get(c) - 1);
+            } else return false;
+        }
+        return true;
+    }
+}
