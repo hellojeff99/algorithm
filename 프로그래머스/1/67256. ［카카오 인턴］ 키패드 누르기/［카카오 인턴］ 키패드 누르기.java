ce7eaed784d0af5a5cf02c393bcef15650aed028
Seongwon Yang <53917860+hellojeff99@users.noreply.github.com>
2026-04-07 17:33:25 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.45 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String solution(int[] numbers, String hand) {
+        StringBuilder sb = new StringBuilder();
+        int cl = 10;
+        int cr = 12;
+        for (int n: numbers) {
+            int num = n;
+            if (num == 0) num = 11;
+            switch (num) {
+                case 1, 4, 7:
+                    sb.append("L");
+                    cl = num;
+                    break;
+                case 3, 6, 9:
+                    sb.append("R");
+                    cr = num;
+                    break;
+                default:
+                    int ld = getDistance(cl, num);
+                    int rd = getDistance(cr, num);
+                    if (ld == rd) {
+                        if (hand.equals("left")) {
+                            sb.append("L");
+                            cl = num;
+                        } else {
+                            sb.append("R");
+                            cr = num;
+                        }
+                    } else if (ld < rd) {
+                        sb.append("L");
+                        cl = num;
+                    } else {
+                        sb.append("R");
+                        cr = num;
+                    }
+            }
+        }
+        return sb.toString();
+    }
+    private int getDistance(int cur, int des) {
+        cur--;
+        des--;
+        int r = Math.abs(cur/3 - des/3);
+        int c = Math.abs(cur%3 - des%3);
+        if (des == 2) {
+            System.out.println(r);
+            System.out.println(c);
+        }
+        return r + c;
+    }
+}
